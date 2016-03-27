f046bb1f9c6180ed3874e1833dcc335e9dcd2040
Nazariy Gorpynyuk <nazariyg@gmail.com>
2016-03-27 16:45:23 +0300

    Initial commit

--- Qcore/System/QSystem.h
+++ Qcore/System/QSystem.h
@@ -1,7 +1,6 @@
 #ifndef QSYSTEM_H
 #define QSYSTEM_H
 
-// Platform-specific information.  The defines to control which platform is included are:
 //   MINGW     : Minimalist GNU for Windows
 //   _WIN32    : Microsoft Windows 9x/2000/XP/Vista
 //   __APPLE__ : Macintosh OS X (10.2.3 or higher required)
@@ -12,7 +11,7 @@
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 #if defined(MINGW)
 
-// Macro used for Microsoft Windows systems to support dynamic link libraries.  Not needed for MINGW.
+// To support dynamic link libraries.
 #define Q_ITEM
 
 typedef long long Int64;
@@ -48,32 +47,18 @@ typedef long long Int64;
 #else
 #define Q_USING_VC80
 
-// many CRT functions at Visual C++ 8.0 have suffix '_s' and are encouraged by the IDE, but for the reason of
-// portability the established ones still remain in use
 #pragma warning( disable : 4996 )
 
 #endif
 
-// The use of Q_ITEM to export an entire class generates warnings when there occur member data or functions
-// that involve templates.  To avoid the warning, Q_ITEM can be applied only to those items that really need to
-// be exported.  The second directive disables the warning "non dll-interface class <a class> used as base for
-// dll-interface class <a class>".
 #pragma warning( disable : 4251 )
 #pragma warning( disable : 4275 )
 
-// Enable the warning: "loop control variable declared in the for-loop is used outside the for-loop scope".
-// The default level 3 warnings do not enable this (level 4 does), but should since allowing the outside scope
-// is a Microsoft extension.
 #pragma warning( error : 4289 )
 
 typedef __int64 Int64;
 
 #endif
-
-// Specialized instantiation of static members in template classes before or after the class itself is
-// instantiated is not a problem with Visual Studio .NET 7.1 and above, but VC 6 likes the specialized
-// instantiation to occur after the class instantiation.
-// #define Q_INSTANTIATE_BEFORE
 
 //------------------------------------------------------------------------------------------------------------------
 // Macintosh OS X platform
@@ -82,14 +67,12 @@ typedef __int64 Int64;
 
 #define Q_BIG_ENDIAN
 
-// Macro used for Microsoft Windows systems to support dynamic link libraries.  Not needed for the Macintosh.
+// To support dynamic link libraries.
 #define Q_ITEM
 
 #include <stdint.h>
 typedef int64_t Int64;
 
-// g++ wants specialized template instantiations to occur after the explicit class instantiations.  CodeWarrior
-// wants them to occur before.
 #ifdef __MWERKS__
 #define Q_INSTANTIATE_BEFORE
 #endif
@@ -99,15 +82,12 @@ typedef int64_t Int64;
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 #else
 
-// Macro used for Microsoft Windows systems to support dynamic link libraries.  Not needed for Linux.
+// To support dynamic link libraries.
 #define Q_ITEM
 
 #include <stdint.h>
 typedef int64_t Int64;
 
-// Linux on a PC.  Red Hat 8.x g++ has problems with specialized instantiation of static members in template
-// classes *before* the class itself is explicitly instantiated.  The problem is not consistent, so the
-// following macro is *not* defined for this platform.
 // #define Q_INSTANTIATE_BEFORE
 
 #endif
